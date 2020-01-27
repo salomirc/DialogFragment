262ad51b300170a4519d6dd59e23c890902a5901
salomirc <36732879+salomirc@users.noreply.github.com>
2020-01-27 19:37:28 +0200

    Dialog fragment part one.

--- app/src/main/java/com/example/dialogfragment/MainActivity.kt
+++ app/src/main/java/com/example/dialogfragment/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.dialogfragment
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.dialogfragment.fragments.CustomDialogFragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+private const val TAG : String = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        openDialogButton.setOnClickListener {
+            Log.d(TAG, "onClick: opening dialog.")
+
+            val dialog = CustomDialogFragment()
+            dialog.show(supportFragmentManager, "MyCustomDialog")
+        }
     }
 }
---
+++ app/src/main/java/com/example/dialogfragment/fragments/CustomDialogFragment.kt
@@ -0,0 +1,72 @@
+package com.example.dialogfragment.fragments
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.LinearLayout
+import android.widget.LinearLayout.*
+import androidx.fragment.app.DialogFragment
+import com.example.dialogfragment.MainActivity
+
+import com.example.dialogfragment.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_custom_dialog.*
+
+private const val TAG : String = "CustomDialogFragment"
+
+class CustomDialogFragment : DialogFragment() {
+    private lateinit var fgmView: View
+    private var listener: OnFragmentInteractionListener? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        fgmView = inflater.inflate(R.layout.fragment_custom_dialog, container, false)
+        return fgmView
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        dialog?.window?.setLayout(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
+        cancelButton.setOnClickListener {
+            Log.d(TAG, "onClick: closing dialog")
+            dialog!!.dismiss()
+        }
+
+        okButton.setOnClickListener {
+            val inputTxt = inputEditText.text.toString()
+            if (inputTxt != "")
+            {
+                (activity as MainActivity).messageTextView.text = inputTxt
+            }
+
+            dialog!!.dismiss()
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            listener = context
+        } else {
+//            throw RuntimeException("$context must implement OnFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    interface OnFragmentInteractionListener {
+        fun onFragmentInteraction(txt: String)
+    }
+
+}
