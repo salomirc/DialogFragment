5fcbc732dae15e2e176d252725171448e8592038
salomirc <36732879+salomirc@users.noreply.github.com>
2020-01-28 13:01:57 +0200

    Dialog fragment part three.

--- app/src/main/java/com/example/dialogfragment/MainActivity.kt
+++ app/src/main/java/com/example/dialogfragment/MainActivity.kt
@@ -7,6 +7,7 @@ import com.example.dialogfragment.fragments.CustomDialogFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 private const val TAG : String = "MainActivity"
+private const val MESSAGE_KEY : String = "messageTextView"
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,11 +15,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if (savedInstanceState != null) {
+            messageTextView.text = savedInstanceState.getString(MESSAGE_KEY)
+        }
+
         openDialogButton.setOnClickListener {
             Log.d(TAG, "onClick: opening dialog.")
 
             val dialog = CustomDialogFragment()
             dialog.show(supportFragmentManager, "MyCustomDialog")
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(MESSAGE_KEY, messageTextView.text.toString())
     }
 }
